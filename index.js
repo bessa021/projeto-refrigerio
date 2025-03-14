79a79debdaebb99e4de9aa6d4bc58b42c89088f4
Alan <allanfbs2@gmail.com>
2025-03-14 19:29:35 -0300

    Atualização no erro de enviar mensagem pelo whatsapp

--- index.js
+++ index.js
@@ -6,14 +6,10 @@ function checkAnimation() {
         const screenPosition = window.innerHeight / 1.3; // Quando o elemento estiver 30% visível na tela
 
         if (elementPosition < screenPosition) {
-            element.classList.add('visible'); // A classe para a animação
+            element.classList.add('animate');
         }
     });
 }
-
-document.getElementById("dropdownButton").addEventListener("click", function() {
-    document.getElementById("dropdownMenu").classList.toggle("hidden");
-});
 
 // Chama a função quando o usuário rolar a página
 window.addEventListener('scroll', checkAnimation);
@@ -27,21 +23,32 @@ document.getElementById("whatsappForm").addEventListener("submit", function(e) {
     const nome = document.getElementById("nome").value;
     const mensagem = document.getElementById("mensagem").value;
 
-    // Verifica se os campos estão preenchidos
-    if (nome && mensagem) {
-        // Codificar a mensagem
-        const mensagemCodificada = encodeURIComponent(`Olá, meu nome é ${nome}.\n${mensagem}`);
+    // Codificar a mensagem
+    const mensagemCodificada = encodeURIComponent(`Olá, meu nome é ${nome}.\n${mensagem}`);
 
-        // Criar o link correto do WhatsApp usando wa.me
-        const whatsappLink = `https://wa.me/5521997327351?text=${mensagemCodificada}`;
+    // Criar o link correto do WhatsApp
+    const whatsappLink = `https://api.whatsapp.com/send?phone=+5521997327351&text=${mensagemCodificada}`;
 
-        // Abrir o WhatsApp com a mensagem
-        window.location.href = whatsappLink; // Usar location.href para não abrir nova aba
-    } else {
-        alert("Por favor, preencha todos os campos.");
-    }
+    // Abrir o WhatsApp com a mensagem
+    window.open(whatsappLink, "_blank");
 });
 
+function checkAnimation() {
+    const elements = document.querySelectorAll('.animate');
+
+    elements.forEach(element => {
+        const elementPosition = element.getBoundingClientRect().top;
+        const screenPosition = window.innerHeight / 1.3; // Quando o elemento estiver 30% visível na tela
+
+        if (elementPosition < screenPosition) {
+            element.classList.add('visible'); // A classe para a animação
+        }
+    });
+}
+
+document.getElementById("dropdownButton").addEventListener("click", function() {
+    document.getElementById("dropdownMenu").classList.toggle("hidden");
+});
 
 function toggleMenu() {
     const menu = document.querySelector('.menu');
